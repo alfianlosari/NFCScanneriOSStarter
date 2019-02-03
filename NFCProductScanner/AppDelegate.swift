0338c81f8dffe05eaf65297197b384cdb68363ef
Alfian Losari <alfianlosari@gmail.com>
2019-02-03 09:31:22 +0700

    Initial Commit

---
+++ NFCProductScanner/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  NFCProductScanner
+//
+//  Created by Alfian Losari on 1/26/19.
+//  Copyright © 2019 Alfian Losari. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+  
+}
+
---
+++ NFCProductScanner/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NFCProductScanner
+//
+//  Created by Alfian Losari on 1/26/19.
+//  Copyright © 2019 Alfian Losari. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBAction func scanTapped(_ sender: Any) {
+
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+}
